18e7fcd9c0e8c5de388222613bb0b3b4d1a945f5
amarlovetips <amarlovetips@gmail.com>
2025-02-13 01:07:44 +0600

    Update config and gitignore

--- utils/config.js
+++ utils/config.js
@@ -1,6 +1,6 @@
 export const config = {
   websiteUrl: process.env.NODE_ENV === 'development' 
     ? 'http://localhost:3000'
-    : 'https://your-actual-vercel-url.vercel.app',
+    : 'https://openmask.vercel.app',
   isDev: process.env.NODE_ENV === 'development'
 }; 
